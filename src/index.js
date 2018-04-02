134792ee0756a23281450cf4ffb8fc240e0cfc1e
axj1l59 <adam_johnston1@homedepot.com>
2018-04-01 20:26:58 -0400

    Adding lodash to see how GCP will handle additional node_modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import { merge } from 'lodash'
+
 export const helloworld = (req, res) => {
+    const x = { some: 1 }
+    const y = { some: 2 }
   // Example input: {"message": "Hello!"}
   if (req.body.message === undefined) {
     // This is an error case, as "message" is required.
@@ -6,6 +10,6 @@ export const helloworld = (req, res) => {
   } else {
     // Everything is okay.
     console.log(req.body.message)
-    res.status(200).send('Success: ' + req.body.message)
+    res.status(200).send('Success: ' + req.body.message + merge(x, y))
   }
 }
